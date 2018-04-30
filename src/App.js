b74a7eeb6686c8595deba1403f9630c347232580
Unknown <bernatesquirol@gmail.com>
2018-04-30 19:41:55 +0200

    related artists

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import queryString from 'query-string'
 import _ from 'lodash';
 import { Stage, Layer, Rect, Text } from "react-konva";
 import Konva from 'konva';
-import Graph2 from './Graph2'
-import Graph from './Graph'
 
+import Graph from './Graph'
+import {AsyncPriorityQueue, AsyncTask} from 'async-priority-queue'
 const paramsToQuery = (params) => {
   let esc = encodeURIComponent;
   let query = Object.keys(params)
@@ -15,40 +15,39 @@ const paramsToQuery = (params) => {
     .join('&');
   return query
 }
-const GetImportance = (range)=>{
-  let returnval = 0
-  switch(range){
-    case 'short_term':
-      returnval = 1
-      break;
-    case 'medium_term':
-      returnval = 3
-      break;
-    case 'long_term':
-      returnval = 4
-      break;
-  }
-  return returnval
-}
+
 
 const containerWidth = window.innerWidth
 const containerHeight = window.innerHeight
+const  queue = new AsyncPriorityQueue({debug:false,maxParallel:1,processingFrequency:20});
 class App extends Component {
   constructor(){
     super();
     this.state = {
       token: null,
       artists: {},
-      createGraph:false
+      artistsCandidates:{},
+      links: {},
+      createGraph:false   
     }
-   
+    
   }
+  checkIfDone(){
+    console.log(queue)
+  }
+
   fetchSpotify(link){
     let me = this
-    
-    return fetch(link,{
-      headers: {'Authorization': 'Bearer '+me.state.token}
-    }).then(response => response.json())
+    let task = new AsyncTask({
+      priority: 'high',
+      callback: function() {
+        return fetch(link,{
+          headers: {'Authorization': 'Bearer '+me.state.token}
+        }).then(response => response.json())
+      }
+    })
+    queue.enqueue(task);
+    return task.promise
   }
   componentDidMount(){
     let parsed = queryString.parse(window.location.search)
@@ -56,14 +55,13 @@ class App extends Component {
     if (access_token) {
       this.setState({
         token:access_token,        
-      },()=>{this.getArtists(50,0,'short_term',
-              ()=>{this.getArtists(50,0,'medium_term',
-                ()=>{this.getArtists(50,0,'long_term',()=>{this.constructGraph()})})
-              })
-            })
-      
+      },()=>{
+        queue.start()
+        this.getArtists(50,0,'short_term')
+        this.getArtists(50,0,'medium_term')
+        this.getArtists(50,0,'long_term')
+      })
     }
-    
   }
   getArtists(limit=50, offset=0, time_range='medium_term', callback=null){    
     let params = {
@@ -73,34 +71,73 @@ class App extends Component {
     };
     let me = this
     let query = paramsToQuery(params)
-    let promisesAll = []
-    let counter = 0
-    let promiseLinksTopArtists = me.fetchSpotify('https://api.spotify.com/v1/me/top/artists?'+query)
+    me.fetchSpotify('https://api.spotify.com/v1/me/top/artists?'+query)
     .then(jsondata=>
+      jsondata['items'] ?
       jsondata['items'].forEach((item,index,array) => {
         let id = item['id']
-        counter++
-        let promiseState = me.setState(prevState => {
+        me.setState(prevState => {
           let artists = {...prevState.artists}
           let newartist = {...artists[id]}
+          let beforetops = newartist.tops || {}
+          beforetops[time_range]=index
+          newartist.tops=beforetops
           newartist.name = item['name']
           newartist.genres = item['genres'],
           newartist.popularity = item['popularity']
-          if (!newartist.importance) newartist.importance = 0
-          newartist.importance += GetImportance(time_range)
+          newartist.order = 0
+          newartist.connections = []
           artists[id] = newartist
-          return {
-            artists: artists
+          
+          return {artists: artists}
+        },()=>me.getArtistDetails(id,2))
+    }):null)
+  }
+  getArtistDetails(id, order){
+    //me.setState((prevState)=>())
+    let me = this
+    me.fetchSpotify('https://api.spotify.com/v1/artists/'+id+'/related-artists')
+    .then(jsondata=>
+      jsondata['artists']?
+      jsondata['artists'].slice(0,10).forEach((item,index,array)=>{
+        let idneighbour = item['uri'].substr(item['uri'].indexOf('artist:')+7)
+        
+        me.setState(prevState => {
+          //let linksartist = prevState.links[idneighbour]?[...prevState.links[idneighbour]]:[]
+          let link = {}
+          let returnstate = {}
+          let artistismain = false
+          let artist = prevState.artists[idneighbour]
+          if(artist==null) artist = prevState.artistsCandidates[idneighbour]
+          else artistismain = true
+          if(artist==null){
+            artist = {}
+            artist.connections = []
+            artist.name =  item['name']
+            artist.genres = item['genres'],
+            artist.popularity = item['popularity']
+            artist.order = order
+            //artistsC[idneighbour] = artist
+            //returnstate.artistsCandidates = artistsC            
           }
+
+          if(artist.connections.indexOf(id)<0 && prevState.artists[id].connections.indexOf(idneighbour)<0 ){
+            artist.connections.push(id)
+          } 
+          if(artistismain){
+            let artists = {...prevState.artists}
+            artists[idneighbour] = artist
+            returnstate.artists = artists
+          }else{
+            let artists = {...prevState.artistsCandidates}
+            artists[idneighbour]=artist
+            returnstate.artistsCandidates = artists
+          }   
+          return returnstate
         })
-        promisesAll.push(promiseState)
-        if(counter==array.length){
-          Promise.all(promisesAll).then(()=>{
-            if(callback) callback()
-          })
-        }
-    }))
-    promisesAll.push(promiseLinksTopArtists)
+              
+      })
+      :null)
   }
   constructGraph(){
     var me = this
@@ -158,7 +195,7 @@ class App extends Component {
         { this.state.token==null?<a href='http://localhost:8888/login' > Login to Spotify </a>:
           null
         }
-        <Graph readyToDraw={this.state.createGraph} height={window.innerHeight} width={window.innerWidth} data={me.state.artists}/>
+        <Graph height={window.innerHeight} createGraph={this.state.createGraph} width={window.innerWidth} artists={me.state.artists}  artistsCandidates={me.state.artistsCandidates}/>
         
       </div>
     );
--- src/Graph.js
+++ src/Graph.js
@@ -3,86 +3,107 @@ import React,{Component}from 'react';
 import Artist from './Artist'
 import {InteractiveForceGraph, ForceGraphNode, ForceGraphLink} from 'react-vis-force';
 import _ from 'lodash';
-const GetRandomPointsR2=(n, width, height)=>{
-    let allpoints=[]
-    for(let i=0;i<n;i++){
-        let point = [Math.random() * width,Math.random() * height]
-        allpoints.push(point)
+
+
+const GetImportance = (range)=>{
+    let returnval = 0
+    switch(range){
+      case 'short_term':
+        returnval = 1
+        break;
+      case 'medium_term':
+        returnval = 3
+        break;
+      case 'long_term':
+        returnval = 4
+        break;
     }
-    return allpoints
-}
+    return returnval
+  }
 
 class Graph2 extends Component {
     constructor(props){
         super(props)
         this.state = {
-            readyToDraw:false
+            
         }
         this.graphRef = React.createRef();
     }    
     static getDerivedStateFromProps(nextProps, prevState){
         let newState = {...prevState}
-        newState.readyToDraw =nextProps.readyToDraw
+        newState.candidatesSelected = Object.keys(nextProps.artistsCandidates).reduce((result, item)=>{
+            if(nextProps.artistsCandidates[item].connections.length > 1) result.push(item)            
+            return result
+        },[])
         return newState
+    }
+    getEdge(id1, id2){
+        return <ForceGraphLink key={id1+'to'+id2} link={{ source: id1, target: id2 }} />
+    }
+    getNode(id, artist,color){
+        return <ForceGraphNode key={id} r={3} node={{ id: id, label: artist['name'] }} fill={color} showLabel/>
     }
     getEdges(){
         let me = this
-        return _.flatMap(Object.keys(me.props.data),(item,index)=>{
-            let edges = me.props.data[item].neighbours.map((item2,index,array)=>{
-                let key2=Object.keys(item2)[0]
-                return <ForceGraphLink strokeWidth={item2[key2]} key={item+'to'+key2} link={{ source: item, target: key2 }} />
+        //let allNodes
+        
+        return _.flatMap(me.state.candidatesSelected,(item,index)=>{
+            let edges = me.props.artistsCandidates[item].connections.map((item2,index,array)=>{
+                return me.getEdge(item,item2)
             })
             return edges    
         })
     }
     getNodes(){
         let me = this
-        return _.map(Object.keys(me.props.data),(item,index)=>
-        {
-            //let node = _.pick(me.props.data[item],['name'])
-            return <ForceGraphNode key={item} r={me.props.data[item]['importance']} node={{ id: item, label: me.props.data[item]['name'] }} fill="red" showLabel/>
-        }
-        )
+        if (_.isEmpty(me.props.artists)) return null
+        let artists = me.props.artists
+        let principal = _.reduce(Object.keys(me.props.artists),(result, item)=>{   
+            result.push(me.getNode(item,me.props.artists[item], "red"))   
+            return result
+        },[])
+        let candidates = _.reduce(me.state.candidatesSelected,(result, item)=>{   
+            result.push(me.getNode(item, me.props.artistsCandidates[item], "blue"))    
+            return result
+        },[])
+        return principal.concat(candidates)
+    }
+    getGraph(nodes, edges){
+      let me = this
+      
+      let stuffinside = nodes
+
+      if (!stuffinside) return null
+      if (edges) stuffinside = stuffinside.concat(edges)
+      if (stuffinside.length == 0) return null      
+      return (<InteractiveForceGraph 
+      ref={this.graphRef}
+      labelAttr="label"
+      highlightDependencies={true}
+      zoom 
+      zoomOptions={{scale:10}}
+      simulationOptions={{ height: me.props.height, width: me.props.width, animate:true }}
+      onSelectNode={(event,node) => {
+          console.log(node['label'])
+          //console.log(event)
+      }}                
+      >
+      {stuffinside}
+      </InteractiveForceGraph>)
     }
     componentDidUpdate(prevProps, prevState, snapshot){
-        if(prevState.readyToDraw ){
-            console.log("yeah")
-            
-        }
+        
     }
     render(){
-        let me = this
-        const nodes = me.state.readyToDraw ? me.getNodes() : null
-        const edges = me.state.readyToDraw ? me.getEdges() : null
-        const graph = me.state.readyToDraw ? (
-            <InteractiveForceGraph 
-                ref={this.graphRef}
-                labelAttr="label"
-                highlightDependencies={true}
-                zoom 
-                zoomOptions={{scale:10}}
-                simulationOptions={{ height: me.props.height, width: me.props.width, animate:true }}
-                onSelectNode={(event,node) => {
-                    console.log(node['label'])
-                    //console.log(event)
-                }}
-                
-            >
-            {nodes}
-            {edges}
-            </InteractiveForceGraph>
-          ) : (
-            null
-          );
-        /*if(this.graphRef.current){
-            console.log(this.graphRef.current)
-        }*/
-          
-        //this.graphRef.zoomTo(0,0,3)
-        return(
-            <div>{graph}
-            </div>
-        )
+      let me = this
+      let nodes = me.props.createGraph?me.getNodes():null
+      let edges = me.props.createGraph?me.getEdges():null
+      let graph = me.props.createGraph?me.getGraph(nodes, edges):null     
+      return(
+          <div>
+          {graph}
+          </div>
+      )
     }
 }
     
